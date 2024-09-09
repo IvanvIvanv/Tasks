d2ea00341fdb500b162983ae85247b9dc3ce8fd8
Yadakk <lolyer47@gmail.com>
2024-09-09 23:19:08 +0300

    Completed the CustomList task

---
+++ Tasks/CustomList.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Tasks
+{
+    public sealed class CustomList<T>
+    {
+        public int Capacity => _array.Length;
+
+        private int _count;
+        public int Count => _count;
+
+        private T[] _array;
+
+        public T this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value;
+        }
+
+        public CustomList()
+        {
+            _array = new T[2];
+        }
+
+        public CustomList(int bufferLength)
+        {
+            _array = new T[bufferLength];
+        }
+
+        public void Add(T value)
+        {
+            IncrementCount();
+            _array[_count - 1] = value;
+        }
+
+        public void Remove()
+        {
+            if (_count == 0) return;
+            _count--;
+        }
+
+        public bool TryInsert(T value, int index)
+        {
+            if (index > _count) return false;
+
+            IncrementCount();
+
+            for (int i = _count - 1; i > index; i--)
+            {
+                _array[i] = _array[i - 1];
+            }
+
+            _array[index] = value;
+
+            return true;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool TryErase(int index)
+        {
+            try
+            {
+                CountOutOfRangeCheck(index);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return false;
+            }
+
+            _array[index] = default!;
+            return true;
+        }
+
+        public bool TryGetAt(int index, out T result)
+        {
+            try
+            {
+                CountOutOfRangeCheck(index);
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                result = default!;
+                return false; 
+            }
+
+            result = _array[index];
+            return true;
+        }
+
+        public bool TryForceCapacity(int newCapacity)
+        {
+            if (newCapacity < 0) return false;
+            ResizeArray(newCapacity);
+            return true;
+        }
+
+        public int Find(T value)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(value, _array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void CountOutOfRangeCheck(int index)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, Count);
+        }
+
+        private void IncrementCount()
+        {
+            if (_count >= Capacity) ResizeArray(Capacity * 2);
+            _count++;
+        }
+
+        private void ResizeArray(int newSize)
+        {
+            Array.Resize(ref _array, newSize);
+            if (_count > Capacity) _count = Capacity;
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            TestList();
+        }
+
+        private static void TestList()
+        {
+            Console.WriteLine(nameof(CustomList<int>));
+            CustomList<int> intList = new(1);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Add));
+            intList.Add(5);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Add));
+            intList.Add(83);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Remove));
+            intList.Remove();
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Remove));
+            intList.Remove();
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryInsert));
+            Console.WriteLine(intList.TryInsert(62, 1));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryInsert));
+            Console.WriteLine(intList.TryInsert(62, 0));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryInsert));
+            Console.WriteLine(intList.TryInsert(14, 0));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryInsert));
+            Console.WriteLine(intList.TryInsert(70, 1));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryErase));
+            Console.WriteLine(intList.TryErase(5));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryErase));
+            Console.WriteLine(intList.TryErase(1));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryGetAt));
+            Console.WriteLine(intList.TryGetAt(5, out int res));
+            Console.WriteLine(res);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryGetAt));
+            Console.WriteLine(intList.TryGetAt(0, out int res2));
+            Console.WriteLine(res2);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryForceCapacity));
+            Console.WriteLine(intList.TryForceCapacity(int.MinValue));
+            Console.WriteLine(intList.Capacity);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.TryForceCapacity));
+            Console.WriteLine(intList.TryForceCapacity(1));
+            Console.WriteLine(intList.Capacity);
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Find));
+            Console.WriteLine(intList.Find(5));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Find));
+            Console.WriteLine(intList.Find(14));
+            PrintAllElements(intList);
+
+            Console.WriteLine(nameof(intList.Clear));
+            intList.Clear();
+            PrintAllElements(intList);
+        }
+
+        private static void PrintAllElements<T>(CustomList<T> customList)
+        {
+            Console.WriteLine("{");
+            for (int i = 0; i < customList.Count; i++)
+            {
+                Console.WriteLine("  " + (customList[i] == null ? "null" : customList[i]!.ToString()) + ',');
+            }
+            Console.WriteLine("}");
+            Console.WriteLine();
+        }
+    }
+}
